0610bb78ef2522f5f4e7856bc4a7c670dbed3a29
NguyenHuyHoang <huyhoang.77.got@gmail.com>
2023-03-10 08:31:05 +0700

    3/10/2023

---
+++ app/src/main/java/nhom2/fpoly/quanlynhahang/MainActivity.java
@@ -0,0 +1,16 @@
+package nhom2.fpoly.quanlynhahang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /// quyết đây
+
+    }
+}
