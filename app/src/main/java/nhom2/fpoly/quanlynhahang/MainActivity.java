8ee0f1e78a8c61cf0ca39b9e2c08838129d47bee
vanquyet00 <117863756+vanquyet00@users.noreply.github.com>
2023-03-10 22:07:28 +0700

    3/10/2023 quyetdey

--- app/src/main/java/nhom2/fpoly/quanlynhahang/MainActivity.java
+++ app/src/main/java/nhom2/fpoly/quanlynhahang/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /// quyết đây
         ///Hoàng
-        //phuc day
+//        phuc day ngu nhu bòmmmm
     }
 }
